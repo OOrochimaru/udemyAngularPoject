bd5adf83cbab1b1fb54648b42fdb92d52d76a696
ryam <ryambudha@gmail.com>
2018-08-28 18:01:51 +0530

    upto binding

--- udemyAngular/src/app/server/server.component.ts
+++ udemyAngular/src/app/server/server.component.ts
@@ -1,9 +1,23 @@
 import {Component} from '@angular/core';
+
+
 @Component({
     selector:'app-server',
     templateUrl:'./server.component.html',
 })
 
 export class ServerComponent{
+    id: Number = 10;
+    isActive = false;
+    serverStatus = 'server is not created yet';
+    constructor(){
+        setTimeout(()=>{
+            this.isActive = true;
+        },2000)
+    }
+
+    onClickEvent(){
+        this.serverStatus = 'server is created now';
+    }
 
 }
